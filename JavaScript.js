c333cdb476f4033ce726cfd9241aee4909af39f3
Adela <strobel.adela@gmail.com>
2026-06-19 16:29:53 +0200

    =

--- JavaScript.js
+++ JavaScript.js
@@ -1,4 +1,8 @@
+/*function anderesBild() {
+    document.getElementById("Hose1").innerHTML = '<img src="https://jadedldn.com/cdn/shop/files/15JANWWECCOM1771_1024x1024.jpg?v=1737999431" alt="Camouflage Hose" style="width:375px;height:500px;">';
 
-for (let i =5; i<5; i-=4 ) { 
-    console.log ("The number is" +i+"<br>" )
-}
+} */
+const Bild = document.getElementById("MeinButton");
+MeinButton.addEventListener("onclick", function () {
+  box.innerHTML = '<img src="https://jadedldn.com/cdn/shop/files/15JANWWECCOM1771_1024x1024.jpg?v=1737999431" alt="Camouflage Hose" style="width:375px;height:500px;">';
+});
